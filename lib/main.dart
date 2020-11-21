107872bfb127f1ee976f69fa330ad52c5e5bbfb9
Samyush <samyushmaharjan@gmail.com>
2020-11-21 16:21:56 +0545

    to do comment

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,8 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  //AI integration not done
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
